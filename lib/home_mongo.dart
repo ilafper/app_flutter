3a794a4a0d696d524b7de3693d073db753dcafd3
ilafper <ivanaalfaya@gmail.com>
2026-04-27 18:54:24 +0200

    diseño targetas hecho y filtro en proceso

--- lib/home_mongo.dart
+++ lib/home_mongo.dart
@@ -26,47 +26,236 @@ class _Mongo_HomeState extends State<Mongo_Home> {
         backgroundColor: Colors.blue,
         title: const Text("Vista Mongo"),
       ),
-      body: Center(
-        child: Container(
-          width: double.infinity,
-          height: double.infinity,
-          decoration: BoxDecoration(
-            border: Border.all(width: 2, color: Colors.black),
+      body: Column(
+        children: [
+          //busqueda y filtro
+          
+          Container(
+            width: double.infinity,
+            padding: const EdgeInsets.all(10),
+            color: const Color.fromARGB(255, 20, 20, 20),
+            child: TextField(
+              style: const TextStyle(color: Colors.white),
+              decoration: InputDecoration(
+                // borde normal
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.yellow, width: 2),
+                ),
+
+                
+                hintText: "Buscar cliente...",
+                hintStyle: const TextStyle(color: Color.fromARGB(224, 255, 241, 43)),
+                prefixIcon: const Icon(Icons.search, color:  Color.fromARGB(224, 255, 241, 43)),
+                filled: true,
+                fillColor: Colors.grey[800],
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide.none,
+                ),
+              ),
+            ),
           ),
-          child: FutureBuilder<List<Cliente>>(
-            future: clientes,
-            builder: (context, snapshot) {
+          Expanded(
+            child: Container(
+              width: double.infinity,
+              height: double.infinity,
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                border: Border.all(width: 2, color: Colors.black),
+                color: Color.fromARGB(255, 24, 23, 23),
+              ),
+              child: FutureBuilder<List<Cliente>>(
+                future: clientes,
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(child: CircularProgressIndicator());
+                  }
 
-             
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return const Center(child: CircularProgressIndicator());
-              }
+                  if (snapshot.hasError) {
+                    return const Center(child: Text("Error cargando datos"));
+                  }
 
-            
-              if (snapshot.hasError) {
-                return const Center(child: Text("Error cargando datos"));
-              }
+                  final data = snapshot.data!;
 
-            
-              final data = snapshot.data!;
-              print("asdasdajsdbAKSJLDaksdASHLDaksjdKASJDlasdAKSD");
-              print("DATA LENGTH: ${data.length}");
-              
-              return ListView.builder(
-                itemCount: data.length,
-                itemBuilder: (context, index) {
-                  return Card(
-                    margin: const EdgeInsets.all(8),
-                    child: ListTile(
-                      leading: const Icon(Icons.person),
-                      title: Text(data[index].nombre),
+                  return SingleChildScrollView(
+                    child: Wrap(
+                      spacing: 10,
+                      runSpacing: 10,
+                      children: List.generate(data.length, (index) {
+                        return SizedBox(
+                          width: 300,
+                          child: Card(
+                            color: const Color.fromARGB(255, 27, 27, 27),
+                            elevation: 5,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(15),
+                            ),
+                            child: Padding(
+                              // padding a toda la targeta
+                              padding: const EdgeInsets.all(15),
+                              child: Column(
+                                children: [
+                                  Container(
+                                    width: double.infinity,
+                                    padding: const EdgeInsets.only(bottom: 10),
+                                    decoration: const BoxDecoration(
+                                      border: Border(
+                                        bottom: BorderSide(
+                                          color: Color.fromARGB(
+                                            255,
+                                            248,
+                                            214,
+                                            23,
+                                          ),
+                                          width: 1,
+                                        ),
+                                      ),
+                                    ),
+                                    child: const Icon(
+                                      Icons.person,
+                                      size: 70,
+                                      color: Color.fromARGB(255, 248, 214, 23),
+                                    ),
+                                  ),
+
+                                  const SizedBox(height: 10),
+
+                                  Text(
+                                    "${data[index].nombre} ${data[index].apellidos}",
+                                    style: const TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.white,
+                                    ),
+                                    textAlign: TextAlign.center,
+                                  ),
+
+                                  const SizedBox(height: 10),
+
+                                  Row(
+                                    children: [
+                                      const Icon(
+                                        Icons.phone,
+                                        size: 18,
+                                        color: Colors.white,
+                                      ),
+                                      const SizedBox(width: 8),
+                                      Text(
+                                        data[index].telefono,
+                                        style: const TextStyle(
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+
+                                  const SizedBox(height: 5),
+
+                                  Row(
+                                    children: [
+                                      const Icon(
+                                        Icons.location_on,
+                                        size: 18,
+                                        color: Colors.white,
+                                      ),
+                                      const SizedBox(width: 8),
+                                      Expanded(
+                                        child: Text(
+                                          data[index].direccion,
+                                          style: const TextStyle(
+                                            color: Colors.white,
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+
+                                  const SizedBox(height: 5),
+
+                                  Row(
+                                    children: [
+                                      const Icon(
+                                        Icons.email,
+                                        size: 18,
+                                        color: Colors.white,
+                                      ),
+                                      const SizedBox(width: 8),
+                                      Expanded(
+                                        child: Text(
+                                          data[index].correo,
+                                          style: const TextStyle(
+                                            color: Colors.white,
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+
+                                  // espaciador
+                                  SizedBox(height: 20),
+
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceEvenly,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.center,
+                                    children: [
+                                      ElevatedButton.icon(
+                                        onPressed: () {},
+                                        icon: const Icon(Icons.delete),
+                                        label: const Text("Borrar"),
+                                        style: ElevatedButton.styleFrom(
+                                          iconColor: Colors.black,
+                                          backgroundColor: Color.fromARGB(
+                                            255,
+                                            248,
+                                            214,
+                                            23,
+                                          ),
+                                          foregroundColor: const Color.fromARGB(
+                                            255,
+                                            0,
+                                            0,
+                                            0,
+                                          ),
+                                        ),
+                                      ),
+
+                                      ElevatedButton.icon(
+                                        onPressed: () {},
+                                        icon: const Icon(Icons.edit),
+                                        label: const Text("Editar"),
+                                        style: ElevatedButton.styleFrom(
+                                          iconColor: Colors.black,
+                                          backgroundColor: Color.fromARGB(
+                                            255,
+                                            248,
+                                            214,
+                                            23,
+                                          ),
+                                          foregroundColor: const Color.fromARGB(
+                                            255,
+                                            0,
+                                            0,
+                                            0,
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        );
+                      }),
                     ),
                   );
                 },
-              );
-            },
+              ),
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
--- lib/index.dart
+++ lib/index.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'home_mongo.dart';
 import 'sql.dart';
 
-
 void main() {
   runApp(const MyApp());
 }
@@ -12,76 +11,98 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: PrimeraVista(),
-    );
+    return const MaterialApp(home: PrimeraVista());
   }
 }
 
-
-
 // guardar la opcion
-class PrimeraVista extends StatefulWidget  {
+class PrimeraVista extends StatefulWidget {
   const PrimeraVista({super.key});
 
   @override
-  State<PrimeraVista> createState()=> Selector();
-
+  State<PrimeraVista> createState() => Selector();
 }
 
 // donde se guarda lo que el usuario escoge
-
-class Selector extends State<PrimeraVista>{
+class Selector extends State<PrimeraVista> {
   String? seleccion;
 
   @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
+      backgroundColor: Color(0xFF2A2A2A),
+
       //barra superior
-      appBar: AppBar(title: const Text("Escoge la base de datos"),),
       //contenido principall
       body: Center(
-        child: DropdownButton<String>(
-          value: seleccion,
-          hint: const Text("Elige una opcion"),
-          
-          // lista de opciones disponibles
-          items: [
-            DropdownMenuItem(
-            value:"MongoDB", 
-            child: Text("Ir a mongo")),
-            DropdownMenuItem(
-            value:"SQL", 
-            child: Text("Ir a sql"))
-          ],
-          onChanged: (value) {
-            setState(() {
-            seleccion=value;
-            });
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(),
+            Container(
+              padding: const EdgeInsets.all(20),
+              margin: const EdgeInsets.only(bottom: 30),
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+              ),
+              child: const Text(
+                "Selecciona una base de datos",
+                // centrar el texto
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: const Color.fromARGB(255, 255, 224, 48),
+                  fontSize: 30,
 
-            if(value== 'MongoDB'){
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+
+            // boton de seleccion
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 12),
+              // color base
+              decoration: BoxDecoration(
+                color: const Color.fromARGB(255, 255, 224, 48),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: DropdownButton<String>(
+                value: seleccion,
+                dropdownColor: const Color.fromARGB(255, 255, 224, 48),
+                style: const TextStyle(color: Color.fromARGB(255, 0, 0, 0)),
+                //icono flecha
+                iconEnabledColor: const Color.fromARGB(255, 0, 0, 0),
+                underline: const SizedBox(), // quita la línea fea
+                items: const [
+                  DropdownMenuItem(value: "MongoDB", child: Text("Ir a mongo")),
+                  DropdownMenuItem(value: "SQL", child: Text("Ir a sql")),
+                ],
+                onChanged: (value) {
+                  setState(() {
+                    seleccion = value;
+                  });
+                  if (value == 'MongoDB') {
             Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => const Mongo_Home(),
-                ),
-              );
-          } else if(value == 'SQL'){
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => const sql_Home(),
-                ),
-              );
+              context,
+              MaterialPageRoute(
+                builder: (context) => const Mongo_Home(),
+              ),
+            );
+          } else if (value == 'SQL') {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const sql_Home(),
+              ),
+            );
           }
-          }
-          
-
-          
+                },
+              ),
+            ),
+          ],
         ),
       ),
     );
   }
 }
-
